87df8a60436d53f74b542c556bbd6c2cbe0a6de6
Thomas Sertorio <thomas.sertorio@gmail.com>
2015-06-23 18:35:41 +0200

    Created Add flat - Style to review

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,4 +1,7 @@
 class PoolsController < ApplicationController
+  #before_action :find_user, only: [:create]
+
+
   def index
     @pools = Pool.all
 
@@ -7,14 +10,37 @@ class PoolsController < ApplicationController
       marker.lat 43.5528470
       marker.lng 7.0173690
     end
+  end
 
   def new
-
+    @pool = Pool.new
   end
 
-
-
-
-
+  def create
+    p '===== debug pool ====='
+    p params = pool_params
+    p @pool = Pool.new(params)
+    p '===== debug current user ====='
+    p current_user
+    p '===== debug assignement ====='
+    p @pool.user = current_user
+    p '===== debug assigned ====='
+    if @pool.save
+      p '===== save! ====='
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+    def find_user
+      @user = current_user
+    end
+
+    def pool_params
+      params.require(:pool).permit(:title, :address, :city, :price, :capacity, :availability, :content)
+    end
+
+
 end
