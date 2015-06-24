a0e658f1a6a68577916d9c65db68b9e345602032
gregoire charles <gcm.charles@gmail.com>
2015-06-24 15:51:30 +0200

    user profile with bookings and requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    @user = current_user
-  end
-
-  def edit
-    @user = current_user
-  end
-
   def update
-    @user = current_user
-    @user.update(user_params)
+    current_user.update(user_params)
     redirect_to user_path
   end
 
@@ -22,3 +13,20 @@ class UsersController < ApplicationController
   end
 
 end
+# <% @myPooRequests.each do |request| %>
+#           <tr>
+#             <td><%= request.users.firs_name %></td>
+#             <td><%= request.users.last_name %></td>
+#             <td><%= request.flats.title %></td>
+#             <td><%= request.status %></td>
+#           </tr>
+#         <% end %>
+        # <% @myRequests.each do |request| %>
+        #   <tr>
+        #     <td><%= request.users.picture %></td>
+        #     <td><%= request.users.first_name + ' ' + request.users.last_name %></td>
+        #     <td><%= request.flats.title %></td>
+        #     <td><%= request.checkin_on %></td>
+        #     <td><%= request.status %></td>
+        #   </tr>
+        # <% end %>
--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,6 +1,7 @@
 class Pool < ActiveRecord::Base
   # belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   belongs_to :user
+  has_many :bookings
 
   validates :title, presence: true
   validates :city, inclusion: {in: %w(Cannes Nice Palavas Valence Avignon Santa-Barbara Miami Monaco Yakutsk), allow_nil: false}
