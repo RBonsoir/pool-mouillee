24573bc0155bd54a7510bb3e358ff7db86b5c00e
gregoire charles <gcm.charles@gmail.com>
2015-06-25 17:25:03 +0200

    user profile quite good

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'bookings/new'
-
-  get 'bookings/create'
-
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   resource :user, only: [:show, :edit, :update]
   resources :pools
-  resources :bookings, only: [:create]
+  resources :bookings, only: [:create, :update]
   root to: 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
